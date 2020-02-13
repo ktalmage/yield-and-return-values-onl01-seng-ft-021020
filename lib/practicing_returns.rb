7e6691fe95baac858b98968fafac25eb775680ce
Kyle Talmage <ktalmage85@gmail.com>
2020-02-13 18:05:55 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,11 +1,11 @@
 def hello(array)
   i = 0
+  collection = []
   while i < array.length
-   binding.pry 
     yield(array[i])
     i += 1
   end
 end
 
-binding.pry
+
 hello(["Tim", "Tom", "Jim"]) { |name|  "Hi, #{name}" }
